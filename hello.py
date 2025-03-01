9839dd3a5fea93f8a6a96ec3cc3898e54636de88
kenichiro yamagata <kenichiro.yamagata@gmail.com>
2025-03-01 14:12:23 +0900

    OK

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-age = 20
+# Date: 2025/03/01
+# Name: 山形　健一郎
+# Description: はじめてのPythonプログラム
+# Version: 1.0
 
-if age >= 18:
-    print("成人だよ")
-elif age == 17:
-    print("もうすぐ成人だよ")
-else:
-    print("未成年だよ")
+apple_pride = 100
+a_type = type(apple_pride)
+
+name = "山形健一郎"
+b_type = type(name)
+
+weight = 60.5
+c_type = type(weight)
+
+print(a_type, b_type, c_type)
+
--- insMeigara_m.sql
+++
@@ -1,5 +0,0 @@
-LOAD DATA INFILE '/home/ec2-user/Py_study/meigara_m.csv' 
-INTO TABLE meigara_m 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n' 
-IGNORE 1 LINES;
